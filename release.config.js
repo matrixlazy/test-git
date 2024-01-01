f8fb1332f5551ced9dbc3eb62790b2bac4dea266
matrix <matrixlazy@gmail.com>
2024-01-01 17:58:20 +0700

    fix: something 57

--- release.config.js
+++ release.config.js
@@ -4,20 +4,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      "semantic-release-slack-bot",
-      {
-        "notifyOnSuccess": false,
-        "notifyOnFail": false,
-        "branchesConfig": [
-          {
-            "pattern": ['master', 'main'],
-            "notifyOnSuccess": true,
-            "notifyOnFail": true
-          }
-        ]
-      }
-    ],
-    [
       '@semantic-release/npm',
       {
         npmPublish: false
@@ -36,6 +22,20 @@ module.exports = {
       '@semantic-release/exec',
       {
         successCmd: `echo "cdn_version=\${nextRelease.version}" >> $GITHUB_OUTPUT`,
+      }
+    ],
+    [
+      "semantic-release-slack-bot",
+      {
+        "notifyOnSuccess": false,
+        "notifyOnFail": false,
+        "branchesConfig": [
+          {
+            "pattern": ['master', 'main'],
+            "notifyOnSuccess": true,
+            "notifyOnFail": true
+          }
+        ]
       }
     ],
   ]
