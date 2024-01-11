36a80bd58c8d960a67a53da045cd70993b24908c
matrix <matrixlazy@gmail.com>
2024-01-11 14:55:54 +0700

    fix: something 60

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['master', 'main'],
+  branches: ['master', 'main', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
